f7f61bbd27c7eb81c6a7eba4cd230350db7e6e5f
Ilya Ustinov <ustin_1999@mail.ru>
2023-03-16 18:01:28 +0300

    update task41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -5,18 +5,28 @@
 int[] CreateArrayIntUser(int size)
 {
     int[] arr = new int[size];
-    Random rnd = new Random();
-    for (int i = 0; i < size; i++)
+    Console.WriteLine($"Введите {size} чисел:");
+    for(int i=0; i<size; i++)
     {
-        arr[i] = rnd.Next(min, max + 1);
+        arr[i] = Convert.ToInt32(Console.ReadLine());
     }
     return arr;
 }
 
 int CountPositiveElements(int[] arr)
 {
+    int countPositiveElements = 0;
     for(int i=0; i<arr.Length; i++)
     {
-        
+        if(arr[i]>0) countPositiveElements++;
     }
+    return countPositiveElements;
 }
+
+Console.Write("Введите количество чисел: ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayIntUser(lenght);
+int count = CountPositiveElements(array);
+Console.WriteLine($"Количество введенных положительных чисел: {count}");
+
+
